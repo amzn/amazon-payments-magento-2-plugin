eefcee8b6eff8d7c9d4e7718a1bac8800e92e0e9
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2016-11-30 19:04:02 -0600

    Add mixin to Amazon to add agreement data to submission

---
+++ src/Payment/view/frontend/web/js/model/place-order-mixin.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*jshint browser:true jquery:true*/
+/*global alert*/
+define([
+    'jquery',
+    'mage/utils/wrapper',
+    'Magento_CheckoutAgreements/js/model/agreements-assigner'
+], function ($, wrapper, agreementsAssigner) {
+    'use strict';
+
+    return function (placeOrderAction) {
+
+        /** Override default place order action and add agreement_ids to request */
+        return wrapper.wrap(placeOrderAction, function (originalAction, paymentData, redirectOnSuccess, messageContainer) {
+            agreementsAssigner(paymentData);
+
+            return originalAction(paymentData, redirectOnSuccess, messageContainer);
+        });
+    };
+});
