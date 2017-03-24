e6df6ab16ff8dc38beb0beab1214a7d95c01f34e
Tanel Raja <tanel@ontapgroup.com>
2017-03-24 09:00:10 +0000

    AMAZON-29: Admin version data added

---
+++ src/Core/Block/Adminhtml/System/Config/Form/Version.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Amazon\Core\Block\Adminhtml\System\Config\Form;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Module\ModuleListInterface;
+
+class Version extends \Magento\Config\Block\System\Config\Form\Field
+{
+    const MODULE_AMAZON_CORE = 'Amazon_Core';
+    const MODULE_AMAZON_LOGIN = 'Amazon_Login';
+    const MODULE_AMAZON_PAYMENT = 'Amazon_Payment';
+
+    protected $_moduleList;
+
+    public function __construct(
+        ModuleListInterface $moduleList,
+        Context $context,
+        array $data = []
+    ) {
+        $this->_moduleList = $moduleList;
+        parent::__construct($context, $data);
+    }
+
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $output = '<div style="background-color:#eee;padding:1em;border:1px solid #ddd;">';
+        $output .= 'Amazon Core version: ' . $this->getVersion(self::MODULE_AMAZON_CORE) . '<br style="display:inherit;" />';
+        $output .= 'Amazon Login version: ' . $this->getVersion(self::MODULE_AMAZON_LOGIN) . '<br style="display:inherit;"  />';
+        $output .= 'Amazon Payment version: ' . $this->getVersion(self::MODULE_AMAZON_PAYMENT);
+        $output .= "</div>";
+         return $output;
+    }
+
+    protected function getVersion($module)
+    {
+        $version = $this->_moduleList->getOne($module);
+        if ($version && isset($version['setup_version'])) {
+            return $version['setup_version'];
+        } else {
+            return 'Missing!';
+        }
+    }
+}
