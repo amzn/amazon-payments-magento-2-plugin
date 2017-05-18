a435f9bd3526c3a742d9701238b808e809e994ee
Tanel Raja <tanel@ontapgroup.com>
2017-05-18 15:19:33 +0100

    AMAZON-29: Comments, internationalisation

--- src/Core/Block/Adminhtml/System/Config/Form/Version.php
+++ src/Core/Block/Adminhtml/System/Config/Form/Version.php
@@ -11,8 +11,17 @@ class Version extends \Magento\Config\Block\System\Config\Form\Field
     const MODULE_AMAZON_LOGIN = 'Amazon_Login';
     const MODULE_AMAZON_PAYMENT = 'Amazon_Payment';
 
+    /**
+     * @var ModuleListInterface
+     */
     protected $_moduleList;
 
+    /**
+     * Version constructor.
+     * @param ModuleListInterface $moduleList
+     * @param Context $context
+     * @param array $data
+     */
     public function __construct(
         ModuleListInterface $moduleList,
         Context $context,
@@ -22,21 +31,29 @@ class Version extends \Magento\Config\Block\System\Config\Form\Field
         parent::__construct($context, $data);
     }
 
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
     public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
         $output = '<div style="background-color:#eee;padding:1em;border:1px solid #ddd;">';
-        $output .= 'Extension version: ' . $this->getVersion(self::MODULE_AMAZON_CORE);
+        $output .= __('Extension version') . ': ' . $this->getVersion(self::MODULE_AMAZON_CORE);
         $output .= "</div>";
          return $output;
     }
 
+    /**
+     * @param $module
+     * @return \Magento\Framework\Phrase
+     */
     protected function getVersion($module)
     {
         $version = $this->_moduleList->getOne($module);
         if ($version && isset($version['setup_version'])) {
             return $version['setup_version'];
         } else {
-            return 'Missing!';
+            return __('Missing!');
         }
     }
 }
