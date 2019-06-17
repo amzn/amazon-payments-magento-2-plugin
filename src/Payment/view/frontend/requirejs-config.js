caeba6d215f551a2201911dbf256929dcdece7d2
Mike Martin <mike@beargroup.com>
2019-06-17 15:23:42 -0400

    ASD-73: remove unused Bluebird library

--- src/Payment/view/frontend/requirejs-config.js
+++ src/Payment/view/frontend/requirejs-config.js
@@ -19,7 +19,6 @@ var config = {
             amazonWidgetsLoader: 'Amazon_Payment/js/amazon-widgets-loader',
             amazonButton: 'Amazon_Payment/js/amazon-button',
             amazonProductAdd: 'Amazon_Payment/js/amazon-product-add',
-            bluebird: 'Amazon_Payment/js/lib/bluebird.min',
             amazonPaymentConfig: 'Amazon_Payment/js/model/amazonPaymentConfig',
             sjcl: 'Amazon_Payment/js/lib/sjcl.min'
         }
