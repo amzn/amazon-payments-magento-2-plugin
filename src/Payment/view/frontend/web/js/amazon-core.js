537141bd33c013939ff1523a64dcbef54a500641
Mike Martin <mike@beargroup.com>
2019-07-25 19:36:03 -0400

    ASD-132: add missing javascript dependencies

--- src/Payment/view/frontend/web/js/amazon-core.js
+++ src/Payment/view/frontend/web/js/amazon-core.js
@@ -18,10 +18,11 @@ define([
     'ko',
     'mage/url',
     'amazonPaymentConfig',
+    'Magento_Ui/js/model/messageList',
     'amazonWidgetsLoader',
     'jquery/jquery-storageapi',
     'mage/cookies'
-], function ($, ko, url, amazonPaymentConfig) {
+], function ($, ko, url, amazonPaymentConfig, messageList) {
     'use strict';
 
     var amazonDefined = ko.observable(false),
@@ -100,11 +101,12 @@ define([
     }
 
     function handleWidgetError(error) {
-        console.log('OffAmazonPayments.Widgets.AddressBook', error.getErrorCode(), error.getErrorMessage());
+        console.log('OffAmazonPayments.Widgets', error.getErrorCode(), error.getErrorMessage());
         switch (error.getErrorCode()) {
             case 'BuyerSessionExpired':
                 messageList.addErrorMessage({message: $.mage.__('Your Amazon session has expired.  Please sign in again by clicking the Amazon Pay Button.')});
-                amazonStorage.amazonlogOut();
+                var storage = require('Amazon_Payment/js/model/storage'); //TODO: clean up this circular dependency
+                storage.amazonlogOut();
                 break;
             case 'ITP':
                 // ITP errors are how handled within the widget code
