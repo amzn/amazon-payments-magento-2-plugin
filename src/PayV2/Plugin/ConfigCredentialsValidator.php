a5ae0ebe048a96ef209e722cbb8cd56e83ef49b8
Ihor Nakonechnyi <ihor@beargroup.com>
2020-05-07 22:13:03 +0000

    ASD-365: Implemented API credentials verification

---
+++ src/PayV2/Plugin/ConfigCredentialsValidator.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace Amazon\PayV2\Plugin;
+
+use Magento\Config\Model\Config;
+use Magento\Store\Model\ScopeInterface;
+
+class ConfigCredentialsValidator
+{
+    const XML_PATH_API_VERSION = 'groups/amazon_payment/fields/api_version/value';
+    const XML_PATH_ACTIVE = 'groups/amazon_payment/groups/credentials/fields/active_v2/value';
+    const XML_PATH_PRIVATE_KEY = 'groups/amazon_payment/groups/credentials/fields/private_key/value';
+    const XML_PATH_PUBLIC_KEY_ID = 'groups/amazon_payment/groups/credentials/fields/public_key_id/value';
+    const XML_PATH_STORE_ID = 'groups/amazon_payment/groups/credentials/fields/store_id/value';
+    const XML_PATH_PAYMENT_REGION = 'groups/amazon_payment/groups/credentials/fields/payment_region/value';
+    const XML_PATH_SANDBOX = 'groups/amazon_payment/groups/credentials/fields/sandbox/value';
+
+    /**
+     * @var \Amazon\PayV2\Model\AmazonConfig
+     */
+    protected $amazonConfig;
+
+    /**
+     * @var \Amazon\PayV2\Client\ClientFactoryInterface
+     */
+    protected $clientFactory;
+
+    /**
+     * @var \Magento\Framework\Message\ManagerInterface
+     */
+    protected $messageManager;
+
+    public function __construct(
+        \Amazon\PayV2\Model\AmazonConfig $amazonConfig,
+        \Amazon\PayV2\Client\ClientFactoryInterface $clientFactory,
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    ) {
+        $this->amazonConfig = $amazonConfig;
+        $this->clientFactory = $clientFactory;
+        $this->messageManager = $messageManager;
+    }
+
+    /**
+     * @param Config $subject
+     * @return boolean
+     */
+    protected function isApplicable(Config $subject)
+    {
+        $result = $subject->getSection() === 'payment';
+        if ($result) {
+            $scope = $subject->getScope() ?: ScopeInterface::SCOPE_STORE;
+            $scopeCode = $subject->getScopeCode();
+
+            $active = $subject->getData(self::XML_PATH_ACTIVE);
+            if ($active === null) {
+                $active = $this->amazonConfig->isActive($scope, $scopeCode);
+            }
+
+            $apiVersion = $subject->getData(self::XML_PATH_API_VERSION);
+            if ($apiVersion === null) {
+                $apiVersion = $this->amazonConfig->getApiVersion($scope, $scopeCode);
+            }
+
+            $result = $active && $apiVersion === '2';
+        }
+        return $result;
+    }
+
+    /**
+     * @param Config $subject
+     * @return boolean
+     */
+    protected function isUpdatedActive(Config $subject)
+    {
+        $scope = $subject->getScope() ?: ScopeInterface::SCOPE_STORE;
+        $scopeCode = $subject->getScopeCode();
+
+        $newValue = $subject->getData(self::XML_PATH_ACTIVE);
+        $oldValue = $this->amazonConfig->isActive($scope, $scopeCode);
+
+        return $newValue && !$oldValue;
+    }
+
+    /**
+     * @param Config $subject
+     * @return boolean
+     */
+    protected function isUpdatedStoreId(Config $subject)
+    {
+        $scope = $subject->getScope() ?: ScopeInterface::SCOPE_STORE;
+        $scopeCode = $subject->getScopeCode();
+
+        $newValue = $subject->getData(self::XML_PATH_STORE_ID);
+        $oldValue = $this->amazonConfig->getClientId($scope, $scopeCode);
+
+        return $newValue !== $oldValue;
+    }
+
+    /**
+     * @param Config $subject
+     * @return array
+     */
+    protected function getUpdatedConfig(Config $subject)
+    {
+        $scope = $subject->getScope() ?: ScopeInterface::SCOPE_STORE;
+        $scopeCode = $subject->getScopeCode();
+
+        $privateKey = $subject->getData(self::XML_PATH_PRIVATE_KEY);
+        if ($privateKey && (preg_match('/^\*+$/', $privateKey) || $privateKey === $this->amazonConfig->getPrivateKey($scope, $scopeCode))) {
+            $privateKey = null;
+        }
+        $publicKeyId = $subject->getData(self::XML_PATH_PUBLIC_KEY_ID);
+        if ($publicKeyId && $publicKeyId === $this->amazonConfig->getPublicKeyId($scope, $scopeCode)) {
+            $publicKeyId = null;
+        }
+        $paymentRegion = $subject->getData(self::XML_PATH_PAYMENT_REGION);
+        if ($paymentRegion && $paymentRegion === $this->amazonConfig->getPaymentRegion($scope, $scopeCode)) {
+            $paymentRegion = null;
+        }
+        $sandbox = $subject->getData(self::XML_PATH_SANDBOX);
+        if ($sandbox !== null) {
+            $sandbox = boolval($sandbox) !== $this->amazonConfig->isSandboxEnabled($scope, $scopeCode) ? boolval($sandbox) : null;
+        }
+
+        return array_filter([
+            'private_key' => $privateKey,
+            'public_key_id' => $publicKeyId,
+            'region' => $paymentRegion,
+            'sandbox' => $sandbox,
+        ]);
+    }
+
+    /**
+     * @param Config $subject
+     * @param array $config
+     * @return $this
+     */
+    protected function validateConfig(Config $subject, array $config = [])
+    {
+        $scope = $subject->getScope() ?: ScopeInterface::SCOPE_STORE;
+        $scopeCode = $subject->getScopeCode();
+
+        $storeId = $subject->getData(self::XML_PATH_STORE_ID);
+        if ($storeId === null) {
+            $storeId = $this->amazonConfig->getClientId($scope, $scopeCode);
+        }
+
+        $client = $this->clientFactory->create($scopeCode, $scope, $config);
+        $response = $client->createCheckoutSession([
+            'webCheckoutDetail' => [
+                'checkoutReviewReturnUrl' => $this->amazonConfig->getCheckoutReviewReturnUrl(),
+            ],
+            'storeId' => $storeId,
+            'platformId' => $this->amazonConfig->getPlatformId(),
+        ], [
+            'x-amz-pay-idempotency-key' => uniqid(),
+        ]);
+
+        if (!in_array($response['status'], [200, 201])) {
+            $data = json_decode($response['response'], true);
+            throw new \Exception($data['message']);
+        }
+        return $this;
+    }
+
+    /**
+     * @param Config $subject
+     * @param string $path
+     * @param mixed $value
+     * @return void
+     */
+    protected function setDataByPath(Config $subject, $path, $value)
+    {
+        $data = $subject->getData();
+        $target = &$data;
+        foreach (explode('/', $path) as $key) {
+            if (!isset($target[$key])) {
+                unset($target);
+                break;
+            }
+            $target = &$target[$key];
+        }
+        if (isset($target)) {
+            $target = $value;
+        }
+        $subject->setData($data);
+        return $this;
+    }
+
+    /**
+     * @param Config $subject
+     * @return null
+     */
+    public function beforeSave(Config $subject)
+    {
+        try {
+            if ($this->isApplicable($subject)) {
+                $config = $this->getUpdatedConfig($subject);
+                if (!empty($config) || $this->isUpdatedActive($subject) || $this->isUpdatedStoreId($subject)) {
+                    $this->validateConfig($subject, $config);
+                }
+            }
+        } catch (\Exception $e) {
+            $this->setDataByPath($subject, self::XML_PATH_ACTIVE, false);
+            $this->messageManager->addErrorMessage(__('Failed to enable Amazon Pay: %1', $e->getMessage()));
+        }
+        return null;
+    }
+}
