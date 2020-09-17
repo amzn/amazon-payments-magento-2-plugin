1b15dc2f993fe6364c8c40086073c15ae975af7b
Jay Becker <jay@beargroup.com>
2020-09-16 20:24:04 -0500

    APF-221 - block split invoicing for DE and UK

--- src/PayV2/Gateway/Config/Config.php
+++ src/PayV2/Gateway/Config/Config.php
@@ -5,6 +5,7 @@
  */
 namespace Amazon\PayV2\Gateway\Config;
 
+use Amazon\PayV2\Model\AmazonConfig;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
@@ -15,6 +16,42 @@ class Config extends \Magento\Payment\Gateway\Config\Config
     const KEY_ACTIVE = 'active';
 
     /**
+     * @var AmazonConfig
+     */
+    protected $amazonConfig;
+
+    /**
+     * @param AmazonConfig $amazonConfig
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        AmazonConfig $amazonConfig,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->amazonConfig = $amazonConfig;
+        parent::__construct($scopeConfig, self::CODE);
+    }
+
+    /**
+     * @param int|null $storeId
+     * @return boolean
+     */
+    protected function canCapturePartial($storeId = null)
+    {
+        $region = $this->amazonConfig->getPaymentRegion(ScopeInterface::SCOPE_STORE, $storeId);
+        switch ($region) {
+            case 'de':
+            case 'uk':
+                $result = false;
+                break;
+            default:
+                $result = parent::getValue('can_capture_partial', $storeId);
+                break;
+        }
+        return $result;
+    }
+
+    /**
      * Gets Payment configuration status.
      *
      * @param int|null $storeId
@@ -23,5 +60,23 @@ class Config extends \Magento\Payment\Gateway\Config\Config
     public function isActive($storeId = null)
     {
         return (bool) $this->getValue(self::KEY_ACTIVE, $storeId);
+    }
+
+    /**
+     * @param string $field
+     * @param int|null $storeId
+     * @return mixed
+     */
+    public function getValue($field, $storeId = null)
+    {
+        switch ($field) {
+            case 'can_capture_partial':
+                $result = $this->canCapturePartial($storeId);
+                break;
+            default:
+                $result = parent::getValue($field, $storeId);
+                break;
+        }
+        return $result;
     }
 }
