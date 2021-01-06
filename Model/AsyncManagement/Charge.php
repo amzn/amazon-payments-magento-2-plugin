20db4b6d2522434ca1ac7a91b523ef2e269443cc
Jay Becker <jay@beargroup.com>
2021-01-06 12:44:31 -0600

    ASD-609 - when capturing payment, make sure that chargeId is set on the invoice and that the invoice is marked as closed

--- Model/AsyncManagement/Charge.php
+++ Model/AsyncManagement/Charge.php
@@ -281,6 +281,7 @@ class Charge extends AbstractOperation
         if ($invoice && ($invoice->canCapture() || $invoice->getOrder()->getStatus() == Order::STATE_PAYMENT_REVIEW)) {
             $order = $invoice->getOrder();
             $payment = $order->getPayment();
+            $invoice->setTransactionId($chargeId);
 
             $invoice->pay();
             $order->addRelatedObject($invoice);
@@ -298,6 +299,7 @@ class Charge extends AbstractOperation
 
             $payment->setDataUsingMethod('base_amount_paid_online', $chargeAmount);
             $payment->addTransactionCommentsToOrder($transaction, $message);
+            $transaction->setIsClosed(true);
             $this->setProcessing($order);
             $order->save();
             $this->asyncLogger->info('Captured Order #' . $order->getIncrementId());
