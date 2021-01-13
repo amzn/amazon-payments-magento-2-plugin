a7487394ff0cbe8c1b4605f69757709f06851842
Jay Becker <jay@beargroup.com>
2021-01-13 14:25:42 -0600

    APF-288 - allow multi-line private keys

--- view/adminhtml/web/js/validation-mixin.js
+++ view/adminhtml/web/js/validation-mixin.js
@@ -35,7 +35,7 @@ define(['jquery'], function ($) {
                 if (v == '******') {
                     return true;
                 }
-                return (/^-----BEGIN RSA PRIVATE KEY-----.*-----END RSA PRIVATE KEY-----$/).test(v);
+                return (/^-----BEGIN RSA PRIVATE KEY-----.*-----END RSA PRIVATE KEY-----$/s).test(v);
             },
             $.mage.__('Private Key field is invalid. It must include header ' +
                 'and footer of the private key. Please check and try again')
