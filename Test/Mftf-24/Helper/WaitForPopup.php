c69cf3716180128ff5aff77021a10c4d373e51f7
Jay Becker <jay@beargroup.com>
2021-06-10 10:50:12 -0500

    APF-311 - phpcs cleanup

--- Test/Mftf-24/Helper/WaitForPopup.php
+++ Test/Mftf-24/Helper/WaitForPopup.php
@@ -2,23 +2,24 @@
 
 namespace Amazon\Pay\Test\Mftf\Helper;
 
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 use Magento\FunctionalTestingFramework\Helper\Helper;
 
-class WaitForPopup extends Helper {
-
-    public function waitForPopup() {
+class WaitForPopup extends Helper
+{
+    public function waitForPopup()
+    {
         /** @var \Magento\FunctionalTestingFramework\Module\MagentoWebDriver $webDriver */
         $webDriver = $this->getModule('\Magento\FunctionalTestingFramework\Module\MagentoWebDriver');
 
         try {
-            $webDriver->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) {
+            $webDriver->executeInSelenium(function (RemoteWebDriver $webdriver) {
                 // Wait for up to 30 seconds, poll every 100ms
-                $webdriver->wait(30, 100)->until(\Facebook\WebDriver\WebDriverExpectedCondition::numberOfWindowsToBe(2));
+                $webdriver->wait(30, 100)->until(WebDriverExpectedCondition::numberOfWindowsToBe(2));
             });
-        } catch(\Exception $e) {
-            print($e);
+        } catch (\Exception $e) {
+            print_r($e);
         }
-        
     }
-
 }
