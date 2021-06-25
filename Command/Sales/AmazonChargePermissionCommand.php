6810cb2f5bf6924b1f4b7401006a8904545009ad
Spencer Gabhart <spencergabhart@pop-os.localdomain>
2021-06-25 09:36:32 -0400

    ASD-720 - Clean up smells and add error checks

--- Command/Sales/AmazonChargePermissionCommand.php
+++ Command/Sales/AmazonChargePermissionCommand.php
@@ -1,20 +1,35 @@
 <?php
+/**
+ * Copyright © Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 namespace Amazon\Pay\Command\Sales;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;    
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Sales\Api\Data\OrderInterface;
 
-class AmazonChargePermissionCommand extends Command {
+class AmazonChargePermissionCommand extends Command
+{
 
     const ORDER_ID = 'orderId';
 
-    /** 
+    /**
      * @var \Magento\Framework\App\State $state
-    */
+     */
     private $state;
 
     /**
@@ -33,7 +48,7 @@ class AmazonChargePermissionCommand extends Command {
     private $searchCriteriaBuilder;
 
     public function __construct(
-        \Magento\Framework\App\State $state, 
+        \Magento\Framework\App\State $state,
         \Magento\Sales\Model\OrderRepository $orderRepository,
         \Amazon\Pay\Model\Adapter\AmazonPayAdapter $amazonAdapter,
         \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
@@ -48,7 +63,7 @@ class AmazonChargePermissionCommand extends Command {
 
     protected function configure()
     {
-        $this->setName('amazon:payment:sales:cp-verify');
+        $this->setName('amazon:payment:sales:verify-charge-permission');
         $this->addOption(
             self::ORDER_ID,
             null,
@@ -68,17 +83,24 @@ class AmazonChargePermissionCommand extends Command {
                 ->addFilter(OrderInterface::INCREMENT_ID, $orderId)->create();
             
             $orderResults = $this->orderRepository->getList($searchCriteria)->getItems();
-            $order = reset($orderResults);
-            $storeId = $order->getStoreId();
-            $chargePermissionId = $order->getPayment()->getAdditionalInformation('charge_permission_id');
+            if (isset($orderResults)) {
+                $order = reset($orderResults);
+                $storeId = $order->getStoreId();
+                $chargePermissionId = $order->getPayment()->getAdditionalInformation('charge_permission_id');
+            }
 
             try {
                 $resp = $this->amazonAdapter->getChargePermission($storeId, $chargePermissionId);
-            } catch (\Exception $ex) { }
+            } catch (\Exception $ex) {
+                $output->writeLn('<error>Error retrieving charge permission from Amazon.</error>');
+            }
 
-            $referenceID = $resp['merchantMetadata']['merchantReferenceId'];
-            $output->write('<info>' . $referenceID . '</info>');
+            if (isset($resp['merchantMetadata']['merchantReferenceId'])) {
+                $referenceID = $resp['merchantMetadata']['merchantReferenceId'];
+            }
+        
+            $verified = $referenceID == $orderId;
+            $output->writeln(var_export($verified));
         }
     }
-
 }
