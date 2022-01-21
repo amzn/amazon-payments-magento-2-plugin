d37648cb9b5dabee77e2f5b84125638f35184ce3
Spencer Gabhart <sgabhart2230@gmail.com>
2022-01-21 14:56:41 -0500

    Fixes for Mftf-24

---
+++ Test/Mftf-24/Helper/AddToCart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Amazon\Pay\Test\Mftf\Helper;
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Magento\FunctionalTestingFramework\Helper\Helper;
+
+class AddToCart extends Helper
+{
+    public function clickAddToCart($addToCartSelector)
+    {
+        /** @var \Magento\FunctionalTestingFramework\Module\MagentoWebDriver $webDriver */
+        $magentoWebDriver = $this->getModule('\Magento\FunctionalTestingFramework\Module\MagentoWebDriver');
+
+        try {
+            $magentoWebDriver->executeInSelenium(function (RemoteWebDriver $remoteWebDriver) use ($addToCartSelector) {
+                $addToCart = WebDriverBy::cssSelector($addToCartSelector);
+                $remoteWebDriver->wait(30, 100)->until(WebDriverExpectedCondition::elementToBeClickable($addToCart));
+            });
+        } catch (\Exception $e) {
+            print_r($e);
+        }
+    }
+}
