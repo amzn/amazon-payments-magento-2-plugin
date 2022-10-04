3ccdb7b451b9c0e6e11effb8745c8f5900b8682e
Spencer Gabhart <sgabhart2230@gmail.com>
2022-10-04 13:32:19 -0400

    ASD-974 - Handle occasional Amazon authentication popup auto-closing, MFTF 2.3/2.4

--- Test/Mftf-24/Helper/LoadAddresses.php
+++ Test/Mftf-24/Helper/LoadAddresses.php
@@ -2,26 +2,51 @@
 
 namespace Amazon\Pay\Test\Mftf\Helper;
 
+use Facebook\WebDriver\Exception\TimeoutException;
 use Magento\FunctionalTestingFramework\Helper\Helper;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 
 class LoadAddresses extends Helper
 {
     public function loadAddresses(
         $changeAddressSelector,
         $addressBackButtonSelector,
-        $defaultAddressSelector
+        $defaultAddressSelector,
+        $openerName,
+        $editShippingButton
     ) {
-        /** @var \Magento\FunctionalTestingFramework\Module\MagentoWebDriver $webDriver */
-        $webDriver = $this->getModule('\Magento\FunctionalTestingFramework\Module\MagentoWebDriver');
-        $waitTime = 15000;
+        /** @var \Magento\FunctionalTestingFramework\Module\MagentoWebDriver $magentoWebDriver */
+        $magentoWebDriver = $this->getModule('\Magento\FunctionalTestingFramework\Module\MagentoWebDriver');
+        $waitTime = 15;
 
         try {
-            $webDriver->waitForElementClickable($changeAddressSelector, $waitTime);
-            $webDriver->click($changeAddressSelector);
-            $webDriver->waitForElementClickable($addressBackButtonSelector, $waitTime);
-            $webDriver->click($addressBackButtonSelector);
+            $magentoWebDriver->waitForElementClickable($changeAddressSelector, $waitTime);
+            $magentoWebDriver->click($changeAddressSelector);
+            $magentoWebDriver->waitForElementClickable($addressBackButtonSelector, $waitTime);
+            $magentoWebDriver->click($addressBackButtonSelector);
 
-            $webDriver->waitForElement($defaultAddressSelector, $waitTime);
+            $magentoWebDriver->waitForElement($defaultAddressSelector, $waitTime);
+        } catch (TimeoutException $e) {
+            $magentoWebDriver->switchToNextTab();
+
+            try {
+                $magentoWebDriver->executeInSelenium(function (RemoteWebDriver $remoteWebDriver) use (
+                    $magentoWebDriver,
+                    $editShippingButton
+                ) {
+                    $editAddressSelector = WebDriverBy::cssSelector($editShippingButton);
+                    $remoteWebDriver->wait(30, 100)->until(WebDriverExpectedCondition::elementToBeClickable($editAddressSelector));
+                    $magentoWebDriver->debug('Click Edit button to return to normal flow');
+                    $remoteWebDriver->findElement($editAddressSelector)->click();
+                    
+                    $remoteWebDriver->wait(30, 100)->until(WebDriverExpectedCondition::numberOfWindowsToBe(2));
+                    $magentoWebDriver->switchToNextTab();
+                });
+            } catch (\Exception $e) {
+
+            }
         } catch (\Exception $e) {
             // Avoid out of memory error sometimes caused by print_r
             // print_r($e);
