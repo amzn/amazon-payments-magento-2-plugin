8629e90d2c184a283c644e4e9ad318744b6c77d9
jajajaime <jaime@beargroup.com>
2023-10-16 15:41:39 -0700

    APF-399 updates interface return type

--- Api/Spc/Response/DeliveryOptionInterface.php
+++ Api/Spc/Response/DeliveryOptionInterface.php
@@ -25,7 +25,7 @@ interface DeliveryOptionInterface
     public function getShippingMethod();
 
     /**
-     * @return array
+     * @return mixed
      */
     public function getShippingEstimate();
 
