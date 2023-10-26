f7e4726f350437c8e1cd0980048375200e71e1ea
Luciano Couget <8595500+lcouget@users.noreply.github.com>
2023-10-26 18:02:39 -0300

    ASD-1152: Added CheckoutSessionDetailsV2 resolver for GraphQl.

---
+++ Model/Resolver/CheckoutSessionDetailsV2.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Amazon\Pay\Model\Resolver;
+
+
+
+use Amazon\Pay\Model\CheckoutSessionManagement;
+use Amazon\Pay\Model\Resolver\CheckoutSessionDetails;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+class CheckoutSessionDetailsV2 extends CheckoutSessionDetails implements ResolverInterface
+{
+
+    /**
+     * CheckoutSessionDetails constructor
+     *
+     * @param CheckoutSessionManagement $checkoutSessionManagement
+     */
+    public function __construct(
+        CheckoutSessionManagement $checkoutSessionManagement
+    ) {
+        parent::__construct($checkoutSessionManagement);
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $amazonSessionId = $args['amazonSessionId'] ?? false;
+        $queryTypes = $args['queryTypes'] ?? false;
+
+        if (!$amazonSessionId) {
+            throw new GraphQlInputException(__('Required parameter "amazonSessionId" is missing'));
+        }
+
+        if (!$queryTypes) {
+            throw new GraphQlInputException(__('Required parameter "queryTypes" is missing'));
+        }
+
+        $response = [];
+        foreach ($queryTypes as $queryType) {
+            $response[$queryType] = $this->getQueryTypesData($amazonSessionId, $queryType) ?:  [];
+        }
+
+        return [
+            'response' => $response
+        ];
+    }
+}
